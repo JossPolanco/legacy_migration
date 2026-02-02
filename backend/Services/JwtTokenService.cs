3c4bba5f846dd892ec294235067d6877badcd622
Josue Polanco <josue224yosi@gmail.com>
2026-02-01 19:37:44 -0600

    Fix a JWT bug.

--- backend/Services/JwtTokenService.cs
+++ backend/Services/JwtTokenService.cs
@@ -17,9 +17,19 @@ namespace Template_API.Services
 
         public string CreateToken(TblUsers user)
         {
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+            var jwtKey = _config["Jwt:Key"] ?? _config["JWT_KEY"] ?? "DefaultJwtSecretKeyForDevelopmentPurposesOnly2024!";
+            var jwtIssuer = _config["Jwt:Issuer"] ?? _config["JWT_ISSUER"] ?? "Template_API";
+            var jwtAudience = _config["Jwt:Audience"] ?? _config["JWT_AUDIENCE"] ?? "Template_API";
+            var expiresMinutes = _config["Jwt:ExpiresMinutes"] ?? "60";
+            
+            if (string.IsNullOrEmpty(jwtKey))
+            {
+                throw new InvalidOperationException("JWT Key is not configured.");
+            }
+            
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var expires = DateTime.UtcNow.AddMinutes(double.Parse(_config["Jwt:ExpiresMinutes"]!));
+            var expires = DateTime.UtcNow.AddMinutes(double.Parse(expiresMinutes));
 
             var claims = new List<Claim>
             {
@@ -28,8 +38,8 @@ namespace Template_API.Services
             };
 
             var token = new JwtSecurityToken(
-                issuer: _config["Jwt:Issuer"],
-                audience: _config["Jwt:Audience"],
+                issuer: jwtIssuer,
+                audience: jwtAudience,
                 claims: claims,
                 expires: expires,
                 signingCredentials: creds
